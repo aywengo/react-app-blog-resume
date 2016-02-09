f252e8f682ebf6c2ce3a40dcae1d2ad736d8071d
Roman Melnyk <aywengo@gmail.com>
2016-02-09 18:36:58 +0100

    fixed prettyDate

--- app/WidgetTwitter.jsx
+++ app/WidgetTwitter.jsx
@@ -6,23 +6,34 @@ var Spinner = require('react-spinner');
 // Takes an ISO time and returns a string representing how
 // long ago the date represents.
 // Copyright (c) 2011 John Resig (ejohn.org)
-function prettyDate(time) {
-    var date = new Date((time || "").replace(/-/g, "/").replace(/[TZ]/g, " ")),
+function prettyDate (time) {
+    var date = time instanceof Date ? time : new Date((time || "").replace(/-/g,"/").replace(/[TZ]/g," ")),
         diff = (((new Date()).getTime() - date.getTime()) / 1000),
         day_diff = Math.floor(diff / 86400);
 
-    if (isNaN(day_diff) || day_diff < 0 || day_diff >= 31)
-        return;
-
-    return day_diff == 0 && (
-        diff < 60 && "just now" ||
-        diff < 120 && "1 minute ago" ||
-        diff < 3600 && Math.floor(diff / 60) + " minutes ago" ||
-        diff < 7200 && "1 hour ago" ||
-        diff < 86400 && Math.floor(diff / 3600) + " hours ago") ||
-        day_diff == 1 && "Yesterday" ||
-        day_diff < 7 && day_diff + " days ago" ||
-        day_diff < 31 && Math.ceil(day_diff / 7) + " weeks ago";
+    if (diff < 60) {
+        return "just now"
+    }
+    else if (diff < 120) {
+        return "1 minute ago";
+    }
+    else if (diff < 3600) {
+        return Math.floor( diff / 60 ) + " minutes ago";
+    } else if (diff < 7200) {
+        return "1 hour ago";
+    } else if (diff < 86400) {
+        return Math.floor( diff / 3600 ) + " hours ago";
+    } else if (day_diff == 1) {
+        return "Yesterday";
+    } else if (day_diff < 7) {
+        return day_diff + " days ago";
+    } else if (day_diff < 31) {
+        return Math.ceil( day_diff / 7 ) + " weeks ago";
+    } else {
+        var months = [ "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December" ];
+        return months[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
+    }
 }
 
 var tweetsBody;
